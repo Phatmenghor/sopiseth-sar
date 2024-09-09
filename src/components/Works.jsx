6f94f859dbbf0695eb73495e48710fd0274a68d7
PhatMenghor <phatmenghor@PhatMenghors-MacBook-Pro.local>
2024-09-09 15:05:05 +0700

    push deploy

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Tilt from "react-tilt";
 import { motion } from "framer-motion";
 
@@ -16,94 +16,220 @@ const ProjectCard = ({
   image,
   source_code_link,
 }) => {
+  const [windowWidth, setWindowWidth] = useState(0);
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <motion.div>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
-      >
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt="project_image"
-            className="w-full h-full object-cover rounded-2xl"
-          />
-          {source_code_link !== "" && (
-            <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
-              <div
-                onClick={() => window.open(source_code_link, "_blank")}
-                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-              >
-                <img
-                  fill="red"
-                  src={link}
-                  className="w-1/2 h-1/2 object-contain "
-                />
-              </div>
+    <>
+      {windowWidth > 640 ? (
+        <motion.div variants={fadeIn("up", "spring", index * 0.5, 0.75)}>
+          <Tilt
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450,
+            }}
+            className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+          >
+            <div className="relative w-full h-[230px]">
+              <img
+                src={image}
+                alt="project_image"
+                className="w-full h-full object-cover rounded-2xl"
+              />
+              {source_code_link !== "" && (
+                <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+                  <div
+                    onClick={() => window.open(source_code_link, "_blank")}
+                    className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                  >
+                    <img
+                      fill="red"
+                      src={link}
+                      className="w-1/2 h-1/2 object-contain "
+                    />
+                  </div>
+                </div>
+              )}
             </div>
-          )}
-        </div>
 
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[20px]">{name}</h3>
-          {/* <p className="mt-2 text-secondary text-[14px]">{description}</p> */}
-          <ul className="mt-5 list-disc ml-4 space-y-2">
-            {description.map((point, index) => (
-              <li
-                key={`experience-point-${index}`}
-                className="text-white-100 text-[14px] pl-1 tracking-wider"
-              >
-                {point}
-              </li>
-            ))}
-          </ul>
-        </div>
+            <div className="mt-5">
+              <h3 className="text-white font-bold text-[20px]">{name}</h3>
+              {/* <p className="mt-2 text-secondary text-[14px]">{description}</p> */}
+              <ul className="mt-5 list-disc ml-4 space-y-2">
+                {description.map((point, index) => (
+                  <li
+                    key={`experience-point-${index}`}
+                    className="text-white-100 text-[14px] pl-1 tracking-wider"
+                  >
+                    {point}
+                  </li>
+                ))}
+              </ul>
+            </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
+            <div className="mt-4 flex flex-wrap gap-2">
+              {tags.map((tag) => (
+                <p
+                  key={`${name}-${tag.name}`}
+                  className={`text-[14px] ${tag.color}`}
+                >
+                  #{tag.name}
+                </p>
+              ))}
+            </div>
+          </Tilt>
+        </motion.div>
+      ) : (
+        <div>
+          <Tilt
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450,
+            }}
+            className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full"
+          >
+            <div className="relative w-full h-[230px]">
+              <img
+                src={image}
+                alt="project_image"
+                className="w-full h-full object-cover rounded-2xl"
+              />
+              {source_code_link !== "" && (
+                <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+                  <div
+                    onClick={() => window.open(source_code_link, "_blank")}
+                    className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                  >
+                    <img
+                      fill="red"
+                      src={link}
+                      className="w-1/2 h-1/2 object-contain "
+                    />
+                  </div>
+                </div>
+              )}
+            </div>
+
+            <div className="mt-5">
+              <h3 className="text-white font-bold text-[20px]">{name}</h3>
+              {/* <p className="mt-2 text-secondary text-[14px]">{description}</p> */}
+              <ul className="mt-5 list-disc ml-4 space-y-2">
+                {description.map((point, index) => (
+                  <li
+                    key={`experience-point-${index}`}
+                    className="text-white-100 text-[14px] pl-1 tracking-wider"
+                  >
+                    {point}
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            <div className="mt-4 flex flex-wrap gap-2">
+              {tags.map((tag) => (
+                <p
+                  key={`${name}-${tag.name}`}
+                  className={`text-[14px] ${tag.color}`}
+                >
+                  #{tag.name}
+                </p>
+              ))}
+            </div>
+          </Tilt>
         </div>
-      </Tilt>
-    </motion.div>
+      )}
+    </>
   );
 };
 
 const Works = () => {
+  const [windowWidth, setWindowWidth] = useState(0);
+
+  useEffect(() => {
+    setWindowWidth(window.innerWidth);
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+    window.addEventListener("resize", handleResize);
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
-      <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText} `}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
-      </motion.div>
+      {windowWidth > 640 ? (
+        <>
+          <motion.div variants={textVariant()}>
+            <p className={`${styles.sectionSubText} `}>My work</p>
+            <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+          </motion.div>
 
-      <div className="w-full flex">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          Projects: the following projects showcase my skills and experience
-          through real-world examples. Each project is briefly described with
-          links to the live versions or apps. Some projects are still in
-          development or confidential. I can provide a full demo if you need
-          more details. Feel free to reach out!
-        </motion.p>
-      </div>
+          <div className="w-full flex">
+            <motion.p
+              variants={fadeIn("", "", 0.1, 1)}
+              className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+            >
+              Projects: the following projects showcase my skills and experience
+              through real-world examples. Each project is briefly described
+              with links to the live versions or apps. Some projects are still
+              in development or confidential. I can provide a full demo if you
+              need more details. Feel free to reach out!
+            </motion.p>
+          </div>
 
-      <div className="mt-20 flex flex-wrap gap-7">
-        {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
-        ))}
-      </div>
+          <div className="mt-20 flex flex-wrap gap-7">
+            {projects.map((project, index) => (
+              <ProjectCard
+                key={`project-${index}`}
+                index={index}
+                {...project}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <>
+          <div>
+            <p className={`${styles.sectionSubText} `}>My work</p>
+            <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+          </div>
+
+          <div className="w-full flex">
+            <p className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
+              Projects: the following projects showcase my skills and experience
+              through real-world examples. Each project is briefly described
+              with links to the live versions or apps. Some projects are still
+              in development or confidential. I can provide a full demo if you
+              need more details. Feel free to reach out!
+            </p>
+          </div>
+
+          <div className="mt-20 flex flex-wrap gap-7">
+            {projects.map((project, index) => (
+              <ProjectCard
+                key={`project-${index}`}
+                index={index}
+                {...project}
+              />
+            ))}
+          </div>
+        </>
+      )}
     </>
   );
 };
